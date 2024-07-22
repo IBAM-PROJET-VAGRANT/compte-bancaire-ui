84e431a8f2f2e8c11867f1324ca634542f5c124a
tatianasawadogo <tatiasawadogo7@gmail.com>
2024-07-22 15:13:41 +0000

    front-end

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -1,10 +1,23 @@
+
+// home.component.ts
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
 
+  searchTransaction() {
+    // Logique pour rechercher une transaction
+    console.log('Rechercher une transaction');
+  }
+
+  addTransaction() {
+    // Logique pour ajouter une transaction
+    console.log('Ajouter une transaction');
+  }
 }
+
